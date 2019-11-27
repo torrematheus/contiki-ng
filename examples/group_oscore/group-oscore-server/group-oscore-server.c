f0bb6f8a9606e5e8804ccb013fea3c3050ec9d42
Martin <martin.gunnarsson@ri.se>
2019-11-27 10:10:10 +0100

    receive group oscore messages from java, can't decrypt

--- examples/group_oscore/group-oscore-server/group-oscore-server.c
+++ examples/group_oscore/group-oscore-server/group-oscore-server.c
@@ -41,6 +41,8 @@
 #include <string.h>
 #include "contiki.h"
 #include "coap-engine.h"
+#include "oscore.h"
+
 
 /* Log configuration */
 #include "sys/log.h"
@@ -53,18 +55,39 @@
 extern coap_resource_t
   res_hello;
 
+uint8_t master_secret[35] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10 };
+uint8_t salt[8] = {0x9e, 0x7c, 0xa9, 0x22, 0x23, 0x78, 0x63, 0x40};
+uint8_t sender_id[1] = { 0x52 };
+uint8_t receiver_id[1] = { 0x25 };
+uint8_t group_id[3] = { 0x44, 0x61, 0x6c };
+
 PROCESS(er_example_server, "Erbium Example Server");
 AUTOSTART_PROCESSES(&er_example_server);
-
 PROCESS_THREAD(er_example_server, ev, data)
 {
   PROCESS_BEGIN();
 
   PROCESS_PAUSE();
 
-  LOG_INFO("Starting Erbium Example Server\n");
+  LOG_INFO("Starting Group OSCORE Server\n");
+
+  oscore_init_server();
+
+  /*Derive an OSCORE-Security-Context. */
+  static oscore_ctx_t *context;
+  context = oscore_derive_ctx(master_secret, 10, salt, 8, 10, sender_id, 1, receiver_id, 1, NULL, 0, OSCORE_DEFAULT_REPLAY_WINDOW);
+  if(!context){
+        printf("Could not create OSCORE Security Context!\n");
+  }
+
+  uint8_t key_id[1] = { 0x25 };
+  oscore_ctx_t *ctx;
+  ctx = oscore_find_ctx_by_rid(key_id, 1);
+  if(ctx == NULL){
+    printf("CONTEXT NOT FOUND\n");
+  }
   
-  coap_activate_resource(&res_hello, "test/hello");
+  coap_activate_resource(&res_hello, "helloWorld");
   
   //multicast initialisation stuff here
   //uip_ip6addr(addr, addr0, addr1, addr2, addr3, addr4, addr5, addr6, addr7)
