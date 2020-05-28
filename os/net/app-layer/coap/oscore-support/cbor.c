acb616d9e8640959d5eddff5d7d8b1afbc0c215e
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-05-28 13:21:23 +0100

    OSCORE: Use Contiki'g logging in cbor

--- os/net/app-layer/coap/oscore-support/cbor.c
+++ os/net/app-layer/coap/oscore-support/cbor.c
@@ -36,20 +36,12 @@
  *
  */
 
-
 #include "cbor.h"
 #include <string.h>
 
-#define DEBUG 0
-#if DEBUG
-#include <stdio.h>
-#define PRINTF(...) printf(__VA_ARGS__)
-#define PRINTF_HEX(data, len)   oscoap_printf_hex(data, len)
-#else
-#define PRINTF(...)
-#define PRINTF_HEX(data, len)
-#endif
-
+#include "coap-log.h"
+#define LOG_MODULE "oscore"
+#define LOG_LEVEL LOG_LEVEL_COAP
 
 int cbor_put_nil(uint8_t **buffer){
 	**buffer = 0xF6;
@@ -83,7 +75,7 @@ cbor_put_array(uint8_t **buffer, uint8_t elements)
 {
   if(elements > 15) {
     /*	#warning "handle this case!\n" */
-    PRINTF("ERROR! in put array\n");
+    LOG_ERR("ERROR! in put array\n");
     return 0;
   }
 
@@ -116,7 +108,7 @@ cbor_put_map(uint8_t **buffer, uint8_t elements)
 {
   if(elements > 15) {
     /*	#warning "handle this case!\n" */
-    PRINTF("ERROR in put map\n");
+    LOG_ERR("ERROR in put map\n");
     return 0;
   }
   **buffer = (0xa0 | elements);
